115b712a5bb099d31711fb52595776acd278ba95
viktoriiarudenko-source <viktoriia.rudenko@mmibordeaux.com>
2026-09-23 15:39:39 +0200

    bdd

---
+++ database/migrations/2026_09_23_132611_create_experiences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('destination_id')->constrained('destinations');
+
+            $table->string('title');
+            $table->text('content');
+            $table->string('photo')->nullable();
+            $table->string('file')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
