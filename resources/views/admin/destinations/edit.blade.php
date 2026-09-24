a150bdec3d61314c09e9d971578e750ab1250d63
viktoriiarudenko-source <viktoriia.rudenko@mmibordeaux.com>
2026-09-24 09:47:55 +0200

    edit

--- resources/views/admin/destinations/edit.blade.php
+++ resources/views/admin/destinations/edit.blade.php
@@ -1,6 +1,8 @@
 <h1>Edit Destination</h1>
 
-<form>
+<form action="/admin/destinations/{{ $destination->id }}" method="POST">
+    @csrf
+
     <label for="name">Name</label>
     <input type="text" id="name" name="name" value="{{ $destination->name }}">
 
