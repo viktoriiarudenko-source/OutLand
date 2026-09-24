10461c7524898bdfe18f3bd461d6a95e71f8e076
maevapiot <maeva.piot@mmibordeaux.com>
2026-09-24 09:52:00 +0200

    Web.php+resources

---
+++ resources/views/experience/create.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un commentaire</title>
+</head>
+
+<body>
+
+    <h1>Ajouter un commentaire</h1>
+
+    <form>
+        
+        <div>
+            <label for="title">Titre</label>
+            <input type="text" id="title" name="title">
+        </div>
+
+        <div>
+            <label for="content">Votre commentaire</label>
+            <textarea id="content" name="content"></textarea>
+        </div>
+
+        <div>
+            <label for="photo">Ajouter une photo</label>
+            <input type="file" id="photo" name="photo">
+        </div>
+
+        <button type="submit">Publier</button>
+
+    </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,27 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DestinationController;
+<?php 
+ 
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\DestinationController; 
 use App\Http\Controllers\Admin\DestinationController as AdminDestinationController;
+use App\Http\Controllers\ExperienceController;
+ 
+Route::get('/', function () { 
+    return view('welcome'); 
+}); 
+ 
+Route::get('/destinations', [DestinationController::class, 'index']); 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/destinations', [DestinationController::class, 'index']);
-
-Route::get('/destinations/{id}', [DestinationController::class, 'show']);
-
-Route::get('/admin/destinations', [AdminDestinationController::class, 'index']);
-
-Route::get('/admin/destinations/create', [AdminDestinationController::class, 'create']);
-
-Route::post('/admin/destinations', [AdminDestinationController::class, 'store']);
-
-Route::get('/admin/destinations/{id}/edit', [AdminDestinationController::class, 'edit']);
-
-Route::post('/admin/destinations/{id}', [AdminDestinationController::class, 'update']);
-
+// Afficher le formulaire pour ajouter un commentaire
+Route::get('/destinations/{destinationId}/experiences/create', [ExperienceController::class, 'create']);
+ 
+Route::get('/destinations/{id}', [DestinationController::class, 'show']); 
+ 
+Route::get('/admin/destinations', [AdminDestinationController::class, 'index']); 
+ 
+Route::get('/admin/destinations/create', [AdminDestinationController::class, 'create']); 
+ 
+Route::post('/admin/destinations', [AdminDestinationController::class, 'store']); 
+ 
+Route::get('/admin/destinations/{id}/edit', [AdminDestinationController::class, 'edit']); 
+ 
+Route::post('/admin/destinations/{id}', [AdminDestinationController::class, 'update']); 
