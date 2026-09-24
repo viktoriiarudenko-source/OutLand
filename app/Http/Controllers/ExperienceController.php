99db9fff623b817b9aec51e43ce7cc50c0250d0f
maevapiot <maeva.piot@mmibordeaux.com>
2026-09-24 09:58:40 +0200

    Experiencecontroller.php

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Experience;
 
 class ExperienceController extends Controller
 {
+    // Affiche le formulaire pour ajouter un commentaire
     public function create($destinationId)
     {
         return view('experiences.create', compact('destinationId'));
+    }
+
+    // Enregistre le commentaire dans la BDD
+    public function store(Request $request, $destinationId)
+    {
+        // Vérifie les informations envoyées par le formulaire
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'photo' => 'nullable|image|max:2048',
+        ]);
+
+        $experience = new Experience();
+
+        $experience->destination_id = $destinationId;
+        $experience->title = $request->title;
+        $experience->content = $request->content;
+
+        // Pour l'instant, on s'occupera de la photo juste après
+        $experience->photo = null;
+
+        // Temporaire : utilisateur 1
+        $experience->user_id = 1;
+
+        $experience->save();
+
+        // Retourne sur la page du pays
+        return redirect('/destinations/' . $destinationId);
     }
 }
